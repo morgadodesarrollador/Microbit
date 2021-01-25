ddb9f710e8ea89648942945cfaa234311cf27e3a
Morgado <morgadodesarrollador@gmail.com>
2021-01-25 16:28:13 +0100

    basicos

---
+++ basicos/IntensidadLuz/indice.ts
@@ -0,0 +1,5 @@
+# reto clase
+Tenemos 2 microbits integradas en un coche. La primera toma la temperatura exterior y cada 3 segundos manda el valor de la temperatura ambiente. En el interior del coche tenemos una segunda microbit que muestra la temperatura exterior del coche. Además del valor de la temperatura mostrará un icono descriptivo. Para temperaturas:
+1. menores que 10º mostrará un copo de nieve
+2. entre 10º y 25º mostrará un sol
+3. mayores que 25º mostrará una llama de fuego
